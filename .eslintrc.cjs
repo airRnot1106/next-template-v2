904119d320d8d212e013ac79d16523b71eac20bc
airRnot1106 <yngw.rintaro@gmail.com>
2023-01-23 16:31:50 +0900

    add eslint plugin for no-restricted-imports

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -95,6 +95,12 @@ module.exports = {
         prefer: 'type-imports',
       },
     ],
+    'no-restricted-imports': [
+      'error',
+      {
+        patterns: ['./*', '../*', '~/*', '~~/*'],
+      },
+    ],
     /* tailwindcss */
     'tailwindcss/no-custom-classname': [
       'warn',
