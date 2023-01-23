83f78c9b0a07a87a2720212e1f849b6e8aee0825
airRnot1106 <yngw.rintaro@gmail.com>
2023-01-23 15:09:59 +0900

    add eslint plugins for tailwindcss

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,12 +4,23 @@ module.exports = {
     'next/core-web-vitals',
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:tailwindcss/recommended',
     'plugin:storybook/recommended',
     'prettier',
   ],
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'tailwindcss'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     sourceType: 'module',
+  },
+  rules: {
+    /* tailwindcss */
+    'tailwindcss/no-custom-classname': [
+      'warn',
+      {
+        config: 'tailwind.config.cjs',
+      },
+    ],
+    'tailwindcss/classnames-order': 'off',
   },
 };
